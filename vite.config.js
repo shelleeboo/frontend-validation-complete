1870cf92ce334b88883f11a2e105e727e6e94642
Shellee Nikole <contact@shellee.nikole.com>
2025-07-13 19:51:01 -0400

    Set up GitHub Pages for main & starter

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base: process.env.BASE_URL ?? '/',   // ← keep this line
+}))
