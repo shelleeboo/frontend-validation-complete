e39413e07a3f9e7194d38d4db5be324e034073ec
Shellee Nikole <contact@shellee.nikole.com>
2025-07-13 21:25:17 -0400

    update navigation title to include validation status

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,7 +13,7 @@ export const Navigation = () => {
   return (
     <div className="border-b bg-slate-100">
       <nav className="container flex justify-between items-center">
-        <div className="font-semibold">React Forms</div>
+        <div className="font-semibold">React Forms - No Validation</div>
         <div className="flex items-center gap-4 text-lg">
           <a href="https://github.com/Yazdun/react-fcc-forms" target="_blank">
             <BsGithub />
